3af7a310fd16a7556d9fb3259c67904cdb1aa08b
jtuluve <jtuluve@gmail.com>
2025-04-16 19:40:39 +0530

    Modified switch, added icon option

--- components/Switch/Switch.tsx
+++ components/Switch/Switch.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, useState } from "react";
+import React, { ChangeEventHandler } from "react";
 import clsx from "clsx";
 
 // Switch Props Interface
@@ -7,6 +7,7 @@ interface SwitchProps {
   onChange?: ChangeEventHandler<HTMLInputElement>;
   disabled?: boolean;
   className?: string;
+  icon?: React.ReactNode; // New prop for custom icon
 }
 
 export const Switch = ({
@@ -14,6 +15,7 @@ export const Switch = ({
   onChange,
   disabled = false,
   className,
+  icon,
 }: SwitchProps) => {
   return (
     <label
@@ -28,7 +30,7 @@ export const Switch = ({
         className={clsx(
           "relative inline-block w-12 h-6 transition-colors duration-300 rounded-full",
           checked ? "bg-blue-600" : "bg-zinc-400",
-          "mr-4" // Add margin-right to create space between the circle and button
+          "mr-4" // Space between the switch and any adjacent content
         )}
       >
         <input
@@ -36,15 +38,24 @@ export const Switch = ({
           checked={checked}
           onChange={onChange}
           disabled={disabled}
-          className="absolute opacity-0 w-full h-full cursor-pointer"
+          className="absolute opacity-0 w-full h-full cursor-pointer z-10"
         />
         <span
           className={clsx(
-            "absolute top-1/2 ml-[2px] -translate-y-1/2 w-5 h-5 bg-white rounded-full transition-transform duration-300 ease-in-out",
-            checked ? "transform translate-x-6" : "",
-            "p-1"
+            "absolute top-1/2 -translate-y-1/2 flex items-center justify-center",
+            "w-5 h-5 bg-white rounded-full",
+            "shadow-sm",
+            "transition-all duration-300 ease-in-out",
+            // The correct positioning with 2px gap
+            checked ? "left-[calc(100%-20px-2px)]" : "left-[2px]"
           )}
-        />
+        >
+          {icon && (
+            <span className="flex items-center justify-center text-gray-600 w-3 h-3">
+              {icon}
+            </span>
+          )}
+        </span>
       </span>
     </label>
   );
