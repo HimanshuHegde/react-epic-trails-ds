05e2f4f19025511637850cfa15b3c48b38897775
jtuluve <jtuluve@gmail.com>
2025-04-16 18:29:55 +0530

    Changed the tile style to black

--- components/Tile/Tile.tsx
+++ components/Tile/Tile.tsx
@@ -10,7 +10,7 @@ export function Tile({
     return (
         <div
             className={
-                "border border-gray-300 rounded-md p-4 transition-all duration-300 hover:shadow-[6px_6px_12px_rgba(0,0,0,0.2)] " +
+                "bg-black border border-gray-700 rounded-md p-4 text-white transition-all duration-300 hover:shadow-[6px_6px_12px_rgba(0,0,0,0.6)] hover:-translate-y-1 " +
                 className
             }
         >
