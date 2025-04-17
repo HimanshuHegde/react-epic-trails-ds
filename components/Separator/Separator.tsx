f521b8a93afaa8cb17dbaa9cae37271bd8ed0b49
jtuluve <jtuluve@gmail.com>
2025-04-17 20:13:09 +0530

    Separator fix

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
-  orientation?: 'horizontal' | 'vertical';
+    orientation?: "horizontal" | "vertical";
 }
 
-const Separator: React.FC<SeparatorProps> = ({
-  orientation = 'horizontal',
-  className,
-  ...rest
+export const Separator: React.FC<SeparatorProps> = ({
+    orientation = "horizontal",
+    className,
+    ...rest
 }) => {
-  const baseStyle = 'bg-zinc-700';
-  const orientationStyle =
-    orientation === 'horizontal'
-      ? 'h-px w-full'
-      : 'w-px h-full';
+    const baseStyle = "bg-zinc-700";
+    const orientationStyle =
+        orientation === "horizontal" ? "h-px w-full" : "w-px";
 
-  return (
-    <div
-      className={clsx(baseStyle, orientationStyle, className)}
-      role="separator"
-      aria-orientation={orientation}
-      {...rest}
-    />
-  );
+    return (
+        <div
+            className={clsx(baseStyle, orientationStyle, className)}
+            role="separator"
+            aria-orientation={orientation}
+            {...rest}
+        />
+    );
 };
-
-export default Separator;
