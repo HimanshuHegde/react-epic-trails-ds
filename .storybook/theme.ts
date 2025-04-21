55b214a708894762b1cba60bd3b113efeab94fb9
jtuluve <jtuluve@gmail.com>
2025-04-21 20:37:23 +0530

    Added brand logo and theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: "light",
+  brandTitle: 'Epic Trails DS components',
+  brandImage: '/Epictrails logo.svg',
+  brandTarget: '_self',
+});
