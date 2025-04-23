dd762b797e31859eb06c112e08aeea49b39b3e87
jtuluve <jtuluve@gmail.com>
2025-04-23 16:06:25 +0530

    refactor(checkbox): improve accessibility and code organization, add useRef for better input handling

--- components/CheckBox/CheckBox.tsx
+++ components/CheckBox/CheckBox.tsx
@@ -1,55 +1,118 @@
-import React, { InputHTMLAttributes, useState } from "react";
+import React, { InputHTMLAttributes, useEffect, useRef, useState } from "react";
 import { Checkbox, SquareOutline } from "../icons";
 type CheckBoxProps = InputHTMLAttributes<HTMLInputElement> & {
-    checked?: boolean
-    children?: React.ReactNode
-    info?:React.ReactNode
-    label?:React.ReactNode
-    error?:boolean
-    disabled?:boolean
-    name?:string
-    value?:string
-}
+    checked?: boolean;
+    children?: React.ReactNode;
+    info?: React.ReactNode;
+    label?: React.ReactNode;
+    error?: boolean;
+    disabled?: boolean;
+    name?: string;
+    value?: string;
+};
 export default function CheckBox({
     children,
     checked,
     info,
     label,
-    error=false,
-    disabled=false,
-    name = 'checkbox',
+    error = false,
+    disabled = false,
+    name = "checkbox",
     value,
     ...props
 }: CheckBoxProps) {
-    const [check , setCheck] = useState<boolean>(checked||false);
+    const [check, setCheck] = useState<boolean>(checked || false);
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        inputRef.current?.onchange?.(new Event("change"));
+    }, [check]);
     return (
-        <div className={`flex gap-2 p-2 ${disabled ? 'cursor-not-allowed text-gray-400' : ''} bg-gray-50 text-black`}>
-
-            <div id={name} className={`${disabled ? 'cursor-not-allowed text-gray-400' : 'cursor-pointer'}`}>
+        <div
+            className={`flex gap-2 p-2 ${
+                disabled ? "cursor-not-allowed text-gray-400" : ""
+            } bg-gray-50 text-black`}
+        >
+            <div
+                className={`${
+                    disabled
+                        ? "cursor-not-allowed text-gray-400"
+                        : "cursor-pointer"
+                }`}
+            >
                 <span className={`flex gap-2`}>
-                    <span className={`  ${disabled ? 'text-gray-400' : ''} flex ${info ? '':'items-center'}`}>
-                        
-                        <input type="checkbox" value={value} className="w-4 h-4 hidden" name={name} id={name}  disabled={disabled} onChange={(e)=>{(setCheck(e.target.checked));}}
-                        {...props} checked={checked??check}/>
+                    <span
+                        className={`  ${disabled ? "text-gray-400" : ""} flex ${
+                            info ? "" : "items-center"
+                        }`}
+                    >
+                        <input
+                            {...props}
+                            type="checkbox"
+                            value={value}
+                            className={
+                                "w-4 h-4 hidden " + (props.className || "")
+                            }
+                            name={name}
+                            disabled={disabled}
+                            checked={checked ?? check}
+                            ref={inputRef}
+                            onChange={(e) => {
+                                setCheck(e.target.checked);
+                                props.onChange?.(e);
+                            }}
+                        />
 
-                        {checked ?? check ?        
-                            <button id={name} className={`flex ${error ? 'text-red-500' : ''} ${disabled ? 'text-gray-400 cursor-not-allowed' : ''}` } onClick={e=>{if(!disabled)(setCheck(!check))}}>
-                                <Checkbox color="black" height="20px"/>
+                        {checked ?? check ? (
+                            <button
+                                aria-label="checkbox"
+                                id={props.id || name}
+                                className={`flex ${
+                                    error ? "text-red-500" : ""
+                                } ${
+                                    disabled
+                                        ? "text-gray-400 cursor-not-allowed"
+                                        : ""
+                                }`}
+                                onClick={() => {
+                                    if (!disabled && checked === undefined) {
+                                        inputRef.current?.click();
+                                    }
+                                }}
+                            >
+                                <Checkbox color="black" height="20px" />
                             </button>
-                            : 
-                            <button id={name} className={`flex ${error ? 'text-red-500' : ''} ${disabled ? 'text-gray-400 cursor-not-allowed' : ''}`} onClick={e=>{if(!disabled)(setCheck(!check))}}>
-                                <SquareOutline color="black"/>
-                            </button>}
+                        ) : (
+                            <button
+                                aria-label="checkbox"
+                                id={props.id || name}
+                                className={`flex ${
+                                    error ? "text-red-500" : ""
+                                } ${
+                                    disabled
+                                        ? "text-gray-400 cursor-not-allowed"
+                                        : ""
+                                }`}
+                                onClick={() => {
+                                    if (!disabled && checked === undefined) {
+                                        inputRef.current?.click();
+                                    }
+                                }}
+                            >
+                                <SquareOutline color="black" />
+                            </button>
+                        )}
                     </span>
-                    <label  htmlFor={name} className="flex flex-col leading-none gap-1 text-black">
+                    <label
+                        htmlFor={name}
+                        className="flex flex-col leading-none gap-1 text-black cursor-pointer"
+                    >
                         {label}
-                        <div className="text-sm text-gray-700 leading-none">{info}</div>
+                        <div className="text-sm text-gray-700 leading-none">
+                            {info}
+                        </div>
                     </label>
                 </span>
             </div>
-                
-                
-            
         </div>
     );
 }
